a5bd7def74f237f791ce90b75c8af75fa55b8508
Alex Jaramillo <91851947+silviojaramillo@users.noreply.github.com>
2023-02-19 14:57:05 -0500

    Update

--- js/control.js
+++ js/control.js
@@ -1,3 +1,15 @@
-function cambiar(cual) {
-    document.getElementById('primero').innerHTML = 'Hola desde JS';
+function rojo(valor) {
+    let color = document.getElementById('primero');
+    color.style.background = 'red';
+    console.log('Primero presionado')
+}
+function amarillo(valor) {
+    let color = document.getElementById('segundo');
+    color.style.background = 'yellow';
+    console.log('Segundo presionado')
+}
+function verde(valor) {
+    let color = document.getElementById('tercero');
+    color.style.background = 'green';
+    console.log('Tercero presionado')
 }
